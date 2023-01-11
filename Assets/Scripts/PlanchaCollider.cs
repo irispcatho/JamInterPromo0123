e0c8a1476647ea2bfab175cd3ef8220801f306f9
ToineF <113095213+ToineF@users.noreply.github.com>
2023-01-11 16:28:27 +0100

    Plancha Collider :curry:

---
+++ Assets/Scripts/PlanchaCollider.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanchaCollider : MonoBehaviour
+{
+    // Variables GD
+    [Header("Variables")]
+    public float GaugeOnPlanchaSpeed;
+    public float GaugeSafeZoneSpeed;
+    private float CurrentGaugeFill;
+    private bool IsColliding;
+    private int UnitSetup = 10000;
+
+    // Text
+    [Header("Text")]
+    public Image GaugeImage;
+
+    private void Update()
+    {
+        CurrentGaugeFill = Mathf.Clamp(CurrentGaugeFill, 0, 1);
+        GaugeImage.fillAmount = CurrentGaugeFill;
+
+        if (IsColliding)
+        {
+            CurrentGaugeFill += GaugeOnPlanchaSpeed / UnitSetup;
+        } else
+        {
+            CurrentGaugeFill -= GaugeSafeZoneSpeed / UnitSetup;
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "Plancha")
+        {
+            IsColliding = true;
+        }
+
+    }
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.tag == "Plancha")
+        {
+            IsColliding = false;
+        }
+
+    }
+}
